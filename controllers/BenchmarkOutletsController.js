fc6ef545afef7559c1e35ff0db507d36b7acd84f
tanvirUpol <tanvirrahmanupol@gmail.com>
2024-11-12 13:47:08 +0600

    updated data for benchmarkl outlet

--- controllers/BenchmarkOutletsController.js
+++ controllers/BenchmarkOutletsController.js
@@ -1,14 +1,17 @@
 const BenchMarkOutletsModel = require("../models/BenchMarkOutletsModel");
 
-// Get by Cluster
 exports.getByCluster = async (req, res) => {
   try {
-    const { clusterCode } = req.body; // Extract clusterCode from request parameters
-    const outlet = await BenchMarkOutletsModel.findOne({ clusterCode }); // Find outlets matching the clusterCode
+    const { clusterCode } = req.body; // Extract clusterCode from request body
+    const outlet = await BenchMarkOutletsModel.findOne({ clusterCode }) // Find outlets matching the clusterCode
+      .sort({ maxSales: -1 }) // Sort by maxSales in descending order
+      .limit(1); // Limit to the highest maxSales outlet
+
     if (!outlet) {
       return res.status(404).json({ message: "No outlets found for this cluster" });
     }
-    res.status(200).json({status:true, outlet}); // Return outlets found
+
+    res.status(200).json({ status: true, outlet }); // Return outlet found
   } catch (error) {
     res.status(500).json({ error: "An error occurred while retrieving outlets" });
   }
